83f8ca5f04356b67e26bb5cc9784d0689b93395a
BKS-deivid <163128890+BKS-deivid@users.noreply.github.com>
2024-05-21 10:02:52 -0300

    Update lista-1.js

--- lista-1/lista-1.js
+++ lista-1/lista-1.js
@@ -1,6 +1,15 @@
-console.log("inicio do programa");
-console.log("fim");
 
-let numero = prompt("1");
-console.log("1");
+const string1 = "inicio do programa";
+const string2 = "fim";
+
+const inicio = string1;
+const fimm = string2;
+
+
+console.log(inicio);
+console.log(fimm);
+
+
+const numero = 1;
+const prompt ();
 
